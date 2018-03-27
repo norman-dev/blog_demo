92e34d82672369c0721c8ef10af3a83ae8b70ce7
Norman Yang <norman.yang@cbxsoftware.com>
2018-03-27 13:19:03 +0800

    Create the Login module

--- blog-ui/src/app/app.module.ts
+++ blog-ui/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { LoginModule } from "./login/login.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
